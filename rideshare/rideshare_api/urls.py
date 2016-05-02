c99463ff7eb919a8b2ec661e98a2f8aac39d730c
JARED SCARR <JAREDSCARR@GMAIL.COM>
2016-05-02 11:18:29 -0700

    updated urls in api to have just one generic view.

--- rideshare/rideshare_api/urls.py
+++ rideshare/rideshare_api/urls.py
@@ -4,6 +4,6 @@ from rideshare_api import views
 
 urlpatterns = [
     url(r'^users/', views.UserEndpoint.as_view(), name='user_endpoint'),
-    url(r'^users/(?P<pk>[0-9]+)/$', views.UserEndpoint.as_view(),
-        name='user_endpoint_detail'),
+    # url(r'^users/(?P<pk>[0-9]+)/$', views.UserEndpoint.as_view(),
+    #     name='user_endpoint_detail'),
 ]
