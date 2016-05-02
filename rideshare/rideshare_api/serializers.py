7fd6ad6e940adc7d5ea838355a66a09d10ff937e
JARED SCARR <JAREDSCARR@GMAIL.COM>
2016-05-02 16:32:15 -0700

    id added to serializer.

--- rideshare/rideshare_api/serializers.py
+++ rideshare/rideshare_api/serializers.py
@@ -8,7 +8,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password',)
+        fields = ('id', 'username', 'email', 'password',)
 
 
 class ProfileSerializer(serializers.ModelSerializer):
