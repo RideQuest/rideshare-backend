3b2e9db7522c948636ce74cb999d640dda2f1a6c
Rick Tesmond <tesmonrd@gmail.com>
2016-05-02 13:15:39 -0700

    added in fields for start and destination addresses

--- rideshare/rideshare_profile/models.py
+++ rideshare/rideshare_profile/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.conf import settings
 from phonenumber_field.modelfields import PhoneNumberField
 
+
 CAR_BRAND = [('Audi', 'Audi'), ('Acura', 'Acura'), ('BMW', 'BMW')]
 
 
@@ -18,6 +19,20 @@ class Profile(models.Model):
     carbrand = models.CharField(max_length=10, choices=CAR_BRAND, default=None)
     carseat = models.IntegerField(max_length=2)
     petsallowed = models.BooleanField()
+
+    # Start Address
+    address_line1 = models.CharField("Address line 1", max_length=40)
+    address_line2 = models.CharField("Address line 2", max_length=40, blank=True)
+    postal_code = models.CharField("Zip Code", max_length=10)
+    city = models.CharField(max_length=50)
+    state = models.CharField("State", max_length=40)
+    # Destination Address
+    destination_address_line1 = models.CharField("Address line 1", max_length=40)
+    destination_address_line2 = models.CharField("Address line 2", max_length=40, blank=True)
+    destination_postal_code = models.CharField("Zip Code", max_length=10)
+    destination_city = models.CharField(max_length=50)
+    destination_state = models.CharField("State", max_length=40)
+
 
     def __str__(self):
         return self.user
