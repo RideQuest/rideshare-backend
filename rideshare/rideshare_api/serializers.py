0ed259d279e7e210c7488782e069ff7a1f2c45f4
Rick Tesmond <tesmonrd@gmail.com>
2016-05-03 13:56:07 -0700

    changed to match

--- rideshare/rideshare_api/serializers.py
+++ rideshare/rideshare_api/serializers.py
@@ -18,9 +18,11 @@ class ProfileSerializer(serializers.ModelSerializer):
         model = Profile
         fields = ('firstname', 'lastname', 'email', 'phonenumber',
                   'carbrand', 'carseat', 'petsallowed')
+
+
+class RouteSerializer(serializers.ModelSerializer):
+    """Serialize route."""
+
     class Meta:
         model = Route
-        fields = ('in_profile', 'address_line1', 'address_line2',
-                  'postal_code', 'city', 'state', 'destination_line1',
-                  'destination_line2', 'destination_postal_code',
-                  'destination_city', 'destination_state')
+        fields = ('id', 'start_point')
