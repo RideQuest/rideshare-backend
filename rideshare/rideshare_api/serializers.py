a01c129b8eab66a4007f46648985f9aba15e0e6a
JARED SCARR <JAREDSCARR@GMAIL.COM>
2016-05-02 17:11:58 -0700

    serializers updated.

--- rideshare/rideshare_api/serializers.py
+++ rideshare/rideshare_api/serializers.py
@@ -16,11 +16,15 @@ class ProfileSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Profile
-        fields = ('firstname', 'lastname', 'email', 'phonenumber',
+        fields = ('id', 'firstname', 'lastname', 'email', 'phonenumber',
                   'carbrand', 'carseat', 'petsallowed')
+
+
+class RouteSerializer(serializers.ModelSerializer):
+    """Serialize route."""
     class Meta:
         model = Route
-        fields = ('in_profile', 'address_line1', 'address_line2',
+        fields = ('id', 'in_profile', 'address_line1', 'address_line2',
                   'postal_code', 'city', 'state', 'destination_line1',
                   'destination_line2', 'destination_postal_code',
                   'destination_city', 'destination_state')
