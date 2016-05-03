d2903fb7fc006d793df0cf57fbf838899e304b3c
Rick Tesmond <tesmonrd@gmail.com>
2016-05-03 14:36:58 -0700

    added url for route edit and auth token given a username

--- rideshare/rideshare_api/urls.py
+++ rideshare/rideshare_api/urls.py
@@ -6,7 +6,11 @@ from rest_framework.authtoken import views as authviews
 urlpatterns = [
     url(r'^profiles/(?P<pk>[0-9]+)/$', views.ProfileEndpoint.as_view(),
         name='profile_endpoint'),
-    url(r'^users/', views.CreateUserEndpoint.as_view(), name='create_user_endpoint'),
+    url(r'^routes/(?P<pk>[0-9]+)/$', views.RouteEndpoint.as_view(),
+        name='route_endpoint'),
+    url(r'^routes/(?P<pk>[0-9]+)/add', views.RouteCreateEndpoint.as_view()),
+    url(r'^users/', views.CreateUserEndpoint.as_view(),
+        name='create_user_endpoint'),
     url(r'^users/(?P<pk>[0-9]+)/$', views.ModifyUserEndpoint.as_view(),
         name='modify_user_endpoint'),
     url(r'^auth-token/', authviews.obtain_auth_token)
