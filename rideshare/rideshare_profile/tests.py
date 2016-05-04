7da34a772603877e10c08dbe77f7b2e2690dd645
GingerQ <qiangwenjing@gmail.com>
2016-05-03 18:32:42 -0700

    model tests initial

--- rideshare/rideshare_profile/tests.py
+++ rideshare/rideshare_profile/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+"""Test User, Profile and model."""
+from django.test import TestCase, Client
+from .models import Profile, Route
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class TestProfile(TestCase):
+    """Test profile functionality."""
+
+    def setUp(self):
+        """Test new user creation."""
+        self.client = Client()
+
+        self.test1 = User.objects.create_user('driver1')
+        self.test2 = User.objects.create_user('rider1')
+        self.test1.save()
+        self.test2.save()
+
+        self.user3 = User.objects.create_user(username='Dear Test', password='pycov')
+        self.user = self.client.login(username='Dear Test', password='pycov')
+
+    def test_verify_save(self):
+        """Test user saved ."""
+        self.assertEquals(len(User.objects.all()), 3)
+
+
+
