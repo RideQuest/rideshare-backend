581ba8cdf9dfebe1bf044e9ebd3c4ccd8b9387d4
JARED SCARR <JAREDSCARR@GMAIL.COM>
2016-05-05 09:04:59 -0700

    trying to format pks

--- rideshare/rideshare_api/tests/test_views.py
+++ rideshare/rideshare_api/tests/test_views.py
@@ -28,6 +28,7 @@ class TestEndpoints(APITestCase):
         self.client = APIClient()
         self.profile = Profile()
         self.user = User.objects.create_user(username='foo', password='foobared')
+
         self.client.force_authenticate(user=self.user)
         self.profile.user = self.user
         self.profile.carbrand = 'Audi'
@@ -39,38 +40,51 @@ class TestEndpoints(APITestCase):
         self.route.start_point = GEOSGeometry('POINT(2 3)', srid=4326)
         self.route.save()
 
+    def tearDown(self):
+        """Teardown."""
+        User.objects.all().delete()
+        Profile.objects.all().delete()
+        Route.objects.all().delete()
+
     # def test_post_user
 
     def test_get_user(self):
-        """Test that a user is created when posted to user endpoint."""
-        response = self.client.get('/users/1/')
+        """Test that a user returned when a get request is performed."""
+        response = self.client.get('/users/{}/').format(self.user.id)
         self.assertEqual(response.data[0]['username'], 'foo')
 
-    def test_get_route(self):
-        """Test that you can get a route."""
-        response = self.client.get('/routes/2/')
-        self.assertEqual(response.data['start_point'],
-                         u'SRID=4326;POINT (2.0000000000000000 3.0000000000000000)')
+    # def test_post_user(self):
+    #     """Test that a user is created when a post request is performed."""
+    #     post = self.client.post('/users/', {'username': 'joe',
+    #                                         'password': 'i love beer'})
+    #     self.assertEquals(post.data['username'], 'joe')
+    #     self.assertEquals(post.data['password'], 'i love beer')
 
-    def test_get_route_no_route_exists(self):
-        """Test that you can get a route."""
-        response = self.client.get('/routes/1/')
-        self.assertEqual(response.data, {u'detail': u'Not found.'})
+    # def test_get_route(self):
+    #     """Test that you can get a route."""
+    #     # this test is passing but presents a problem that if moved 
+    #     # the test pk will fail with a KeyError
+    #     response = self.client.get('/routes/2/')
+    #     self.assertEqual(response.data['start_point'],
+    #                      u'SRID=4326;POINT (2.0000000000000000 3.0000000000000000)')
 
-    def test_get_profile(self):
-        """Test that you can get a profile."""
-        response = self.client.get('/profiles/1/')
-        self.assertEqual(response.data['carbrand'], 'Audi')
+    # def test_get_route_no_route_exists(self):
+    #     """Test that you can get a route."""
+    #     response = self.client.get('/routes/1/')
+    #     self.assertEqual(response.data, {u'detail': u'Not found.'})
 
-    def test_add_route(self):
-        """Test that a post to /routes/add/ behaves as expected."""
-        post = self.client.post('/routes/add/')
-        import pdb; pdb.set_trace()
+    # def test_get_profile(self):
+    #     """Test that you can get a profile."""
+    #     response = self.client.get('/profiles/1/')
+    #     self.assertEqual(response.data['carbrand'], 'Audi')
 
     # def test_add_route(self):
     #     """Test that a post to /routes/add/ behaves as expected."""
-    #     post = self.client.get('/routes/add/')
-        
+    #     post = self.client.post('/routes/add/', {'user': 1,
+    #                                              'start_point': u'SRID=4326;POINT (2.0000000000000000 3.0000000000000000)'})
+    #     get = self.client.get('/routes/1/')
+    #     self.assertEquals(get.data['user'], 1)
+  
 
 
 # class ModifyUserEndpoint(generics.RetrieveUpdateDestroyAPIView):
