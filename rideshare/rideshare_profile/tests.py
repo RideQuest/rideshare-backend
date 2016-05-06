74fe278c9d23c152834adebddeac9704ea2cb9f4
GingerQ <qiangwenjing@gmail.com>
2016-05-05 17:36:38 -0700

    model testing is hard.

--- rideshare/rideshare_profile/tests.py
+++ rideshare/rideshare_profile/tests.py
@@ -1,5 +1,5 @@
 """Test User, Profile and model."""
-from django.test import TestCase, Client
+from django.test import TestCase
 from .models import Profile, Route
 from django.contrib.auth.models import User
 from django.contrib.gis.geos import GEOSGeometry
@@ -9,52 +9,71 @@ class TestProfile(TestCase):
     """Test profile functionality."""
 
     def setUp(self):
-        """Test new user creation."""
-        self.client = Client()
+        """Test new profile creation."""
+        self.user_1 = User.objects.create_user('driver1')
+        self.user_2 = User.objects.create_user('rider1')
+        self.user_3 = User.objects.create_user('rider2')
 
-        self.test_user_1 = User.objects.create_user('driver1')
-        self.test_user_2 = User.objects.create_user('rider1')
-        self.test_user_3 = User.objects.create_user(username='Dear Test', password='pycov')
-        self.test_profile_1 = Profile()
-        self.test_profile_2 = Profile()
-        self.test_profile_3 = Profile()
-        self.test_profile_1.user = self.test_user_1
-        self.test_profile_2.user = self.test_user_2
-        self.test_profile_3.user = self.test_user_3
-        self.test_profile_1.carbrand = 'Audi'
-        self.test_profile_2.carbrand = 'Audi'
-        self.test_profile_3.carbrand = 'Audi'
-        self.test_profile_1.carseat = 3
-        self.test_profile_2.carseat = 3
-        self.test_profile_3.carseat = 3
-        self.test_profile_1.petsallowed = True
-        self.test_profile_2.petsallowed = True
-        self.test_profile_3.petsallowed = True
-        self.test_profile_1.save()
-        self.test_profile_2.save()
-        self.test_profile_3.save()
-        self.test_route_1 = Route()
-        self.test_route_2 = Route()
-        self.test_route_3 = Route()
-        self.test_route_1.user = self.test_profile_1
-        self.test_route_2.user = self.test_profile_2
-        self.test_route_3.user = self.test_profile_3
-        self.test_route_1.start_point = GEOSGeometry('POINT(1 2)', srid=4326)
-        self.test_route_2.start_point = GEOSGeometry('POINT(1 2)', srid=4326)
-        self.test_route_3.start_point = GEOSGeometry('POINT(1 2)', srid=4326)
-        self.test_route_1.save()
-        self.test_route_2.save()
-        self.test_route_3.save()
+        self.profile_1 = Profile.objects.create(user=self.user_1,
+                                                firstname="iwantto",
+                                                lastname="addprofile",
+                                                email="letmeadd@email.com",
+                                                phonenumber="",
+                                                carbrand="Audi",
+                                                carseat=3,
+                                                petsallowed="f"
+                                                )
+        self.profile_2 = Profile.objects.create(user=self.user_2,
+                                                firstname="iwantto",
+                                                lastname="addprofile",
+                                                email="letmeadd@email.com",
+                                                phonenumber="",
+                                                carbrand="Audi",
+                                                carseat=3,
+                                                petsallowed="f"
+                                                )
+        self.profile_1.save()
+        self.profile_2.save()
+
+        self.route_1 = Route()
+        self.route_2 = Route()
+        self.route_1.user = self.profile_1
+        self.route_2.user = self.profile_2
+        self.route_1.start_point = GEOSGeometry('POINT(1 2)', srid=4326)
+        self.route_2.start_point = GEOSGeometry('POINT(3 4)', srid=4326)
+        self.route_1.save()
+        self.route_2.save()
 
     def test_verify_save(self):
         """Test user saved ."""
         self.assertEquals(len(User.objects.all()), 3)
 
     def test_profile_count(self):
-        self.assertEquals(len(Profile.objects.all()), 3)
+        """Test profiles saved."""
+        self.assertEquals(len(Profile.objects.all()), 2)
 
     def test_route_count(self):
-        self.assertEquals(len(Route.objects.all()), 3)
+        """Test routes saved."""
+        self.assertEquals(len(Route.objects.all()), 2)
 
+    def test_user_profile(self):
+        """Test profiles are connected to users."""
+        assert self.profile_1.user == self.user_1
+        assert self.profile_2.user == self.user_2
+
+    def test_profile_route(self):
+        """Test route are connected to profiles."""
+        assert self.route_1.user == self.profile_1
+        assert self.route_2.user == self.profile_2
+
+    def test_profile_field(self):
+        """Test required fields are provided."""
+        self.assertNotEqual('a', self.profile_1.firstname)
+        self.assertNotEqual('a', self.profile_1.lastname)
+        self.assertNotEqual('a', self.profile_1.email)
+        self.assertNotEqual('a', self.profile_1.phonenumber)
+        self.assertNotEqual('a', self.profile_1.carbrand)
+        self.assertNotEqual('a', self.profile_1.carseat)
+        self.assertNotEqual('t', self.profile_1.petsallowed)
 
 
